556b306770328313ae4d12b397c8aaceefa93bda
juancruzgauna <juancruzgauna97@gmail.com>
2025-03-31 18:20:20 -0300

    Se agrega clase profesor, sus respectivos atributos, getters y setters y se aplica el metodo de encapsulamiento

---
+++ src/ejercicio1/Profesor.java
@@ -0,0 +1,20 @@
+package ejercicio1;
+
+public class Profesor extends Empleado {
+	private String cargo;
+	private int antiguedadDocente;
+	
+	
+	public String getCargo() {
+		return cargo;
+	}
+	public void setCargo(String cargo) {
+		this.cargo = cargo;
+	}
+	public int getAntiguedadDocente() {
+		return antiguedadDocente;
+	}
+	public void setAntiguedadDocente(int antiguedadDocente) {
+		this.antiguedadDocente = antiguedadDocente;
+	}
+}
