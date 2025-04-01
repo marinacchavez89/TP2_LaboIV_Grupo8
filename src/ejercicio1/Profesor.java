cb76a02dc5a12c833c55d11c0674dabe63c145af
Mcanita <albadebiase@gmail.com>
2025-03-31 21:24:08 -0300

    Se agrega los constructores a la clase Profesor

--- src/ejercicio1/Profesor.java
+++ src/ejercicio1/Profesor.java
@@ -4,7 +4,23 @@ public class Profesor extends Empleado {
 	private String cargo;
 	private int antiguedadDocente;
 	
+	// constructores 
 	
+	public Profesor ()
+	{
+		super();
+		cargo = "sin cargo";
+		antiguedadDocente = 0 ;
+	}
+	
+	public Profesor(String cargo, int antiguedadDocente)
+	{
+		super();
+		this.cargo = cargo;
+		this.antiguedadDocente = antiguedadDocente;
+	}
+	
+	//set y get
 	public String getCargo() {
 		return cargo;
 	}
